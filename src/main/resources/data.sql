45607699ff2bc7ed60801b364bf46b6817f9db7c
icambraa <126367844+icambraa@users.noreply.github.com>
2025-03-30 22:46:46 +0200

    data.sql añadido

---
+++ src/main/resources/data.sql
@@ -0,0 +1,135 @@
+INSERT INTO lineups (
+  abilities,
+  affected_area,
+  agent,
+  description,
+  executed_on,
+  image_url,
+  map,
+  side,
+  title,
+  upload_date,
+  uploaded_by,
+  video_url
+)
+VALUES
+  ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8'),
+
+    ('Dark Cover', 'B main', 'Omen', 'Lineup desde main de b a side', 'Side de b',
+  'https://res.cloudinary.com/dsq2zzlt2/image/upload/v1743356376/j3qk6jzk1f7lgtjxgzfa.webp',
+  'Haven', 'attack', 'Omen usando Dark Cover desde Side de b hacia B main',
+  '2025-03-30 19:39:34.499785', 'prueba@gmail.com', 'https://www.youtube.com/shorts/_Pzf2lRaPm8');
+
